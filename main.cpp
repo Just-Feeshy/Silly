cb7c3e8e06d086e4570d53c65d94890d067a7433
Feeshy Fesh <58647349+Just-Feeshy@users.noreply.github.com>
2022-02-24 17:17:26 +0000

    WHOLE NEW SILLY!

--- main.cpp
+++ main.cpp
@@ -1,29 +1,6 @@
-#include <bits/stdc++.h>
-
-#include <iostream>
-#include <cmath>
-#include <vector>
+#include "GetSilly.hpp"
 
 int main() {
-    std::vector<int> silly;
-
-    silly.push_back(0);
-    silly.push_back(1);
-    silly.push_back(2);
-    silly.push_back(6);
-    silly.push_back(3);
-    silly.push_back(4);
-
-    sort(silly.begin(), silly.end());
-
-    for(int i=0; i<silly.size(); i++) {
-        printf("%d", silly[i]);
-    }
-
-    int sillyTwo[2];
-
-    sillyTwo[0] = 5;
-
-    std::cout << "" << std::endl;
-    std::cout << sillyTwo[0] << std::endl;
+    GetSilly s;
+    s.contactSilly();
 }
